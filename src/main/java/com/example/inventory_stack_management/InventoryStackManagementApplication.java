725b21f6c7111f10d4fc5409c1e9eb136e364bfa
IT24102384 <it24102384@my.sliit.lk>
2025-05-19 12:50:53 +0530

    Add files via upload
    creating new brnch inventory management

---
+++ src/main/java/com/example/inventory_stack_management/InventoryStackManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.inventory_stack_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryStackManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryStackManagementApplication.class, args);
+	}
+
+}
